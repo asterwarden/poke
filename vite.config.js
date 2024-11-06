7173054f73a9682d1c9d613e626f8770bc0517c4
mav <ar.maklakov@gmail.com>
2024-11-06 21:18:18 +0300

    cleanup: remove create-react-app

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: "packages",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  server: {
-    open: "/",
-  },
-});
+})
